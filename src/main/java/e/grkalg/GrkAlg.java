5250638e84aedd866849c921c239fbd65e0d5944
aleksey-kochetkov <mail-kochetkov@yandex.ru>
2018-05-28 22:08:45 +0300

    Quick Sort

--- src/main/java/e/grkalg/GrkAlg.java
+++ src/main/java/e/grkalg/GrkAlg.java
@@ -1,6 +1,9 @@
 package e.grkalg;
 
+import java.util.Iterator;
+
 public class GrkAlg {
+
   public static void selectionSort(int[] array) {
     int sorted = 0;
     while (sorted < array.length) {
@@ -13,5 +16,23 @@ public class GrkAlg {
       array[lowI] = tmp;
     }
   }
-  
+
+    public static void quickSort(int[] array, int left, int right) {
+        if (left < right) {
+            int pivot = array[right];
+            int bound = left;
+            int tmp;
+            for (int i = left; i < right; i++) {
+                if (array[i] < pivot) {
+                    tmp = array[bound];
+                    array[bound++] = array[i];
+                    array[i] = tmp;
+                }
+            }
+            array[right] = array[bound];
+            array[bound] = pivot;
+            quickSort(array, left, bound - 1);
+            quickSort(array, bound + 1, right);
+        }
+    }
 }
--- src/test/java/e/grkalg/GrkAlgTest.java
+++ src/test/java/e/grkalg/GrkAlgTest.java
@@ -1,9 +1,12 @@
 package e.grkalg;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 import org.junit.Test;
 import e.Output;
+import java.util.List;
+import java.util.Arrays;
 
 public class GrkAlgTest {
 
@@ -15,8 +18,11 @@ public class GrkAlgTest {
     assertThat(input, is(expect));
   }
 
-  @Test
-  public void whenTmp() {
-    Output.println("Текст кириллицей");
-  }
+    @Test
+    public void whenQuickSort() {
+        int[] array = {1, 2, 13, 2, 2, 3, 11, 3, 3, 14, 1, 1};
+        int[] expect = {1, 1, 1, 2, 2, 2, 3, 3, 3, 11, 13, 14};
+        GrkAlg.quickSort(array, 0, array.length - 1);
+        assertThat(array, is(expect));
+    }
 }
