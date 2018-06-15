58e419dbd13be71674a770d19e20f8c6035d9c7e
aleksey-kochetkov <mail-kochetkov@yandex.ru>
2018-06-15 16:11:23 +0300

    Simple primality test

---
+++ src/main/java/e/exercise/PrimalityTest.java
@@ -0,0 +1,26 @@
+package e.exercise;
+
+/**
+ * Primality test.
+ */
+public class PrimalityTest {
+    public boolean isPrime(int n) {
+        boolean result;
+        if (n <= 1) {
+            result = false;
+        } else if (n <= 3) {
+            result = true;
+        } else {
+            result = n % 2 != 0;
+            if (result) {
+                for (int i = 3; i * i <= n; i += 2) {
+                    if (n % i == 0) {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
